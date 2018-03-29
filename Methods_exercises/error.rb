8d8bbbbba73889e9a94faad018d4e77704d791b1
Michael Frederick <michaelfrederick@Michaels-MacBook-Pro.local>
2018-03-29 16:33:52 -0400

    Methods chapter

---
+++ Methods_exercises/error.rb
@@ -0,0 +1,5 @@
+=begin 
+The method 'calculate_product' needs 
+two arguments to work, and it has only been 
+given one.
+=end 
---
+++ Methods_exercises/greeting.rb
@@ -0,0 +1,5 @@
+def greeting(name)
+  "Hello, #{name}! Nice to meet you!"
+end
+
+puts greeting("Mike")
